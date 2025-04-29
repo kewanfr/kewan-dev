e1b45dc0a10e3755e17f430d4952d3d9083ff81d
Kéwan B <mail@kewan.fr>
2025-04-29 14:24:44 +0200

    update postcss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,7 @@
+/** @type {import('postcss').Config} */
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
 };
