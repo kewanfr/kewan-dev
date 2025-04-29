244952eda88b2e5da83b27f43c7ac7fdd5a53e1a
Kéwan B <mail@kewan.fr>
2025-04-29 14:18:18 +0200

    pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+
+function About() {
+  return (
+    <>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Bienvenue sur ma page Tailwind + Vite !
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default About;
