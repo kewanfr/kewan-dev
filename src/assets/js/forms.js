e923998f4a562e0307737cb4b33e0e0c9663cc0b
Kéwan B <mail@kewan.fr>
2026-01-30 16:49:45 +0100

    fix

--- src/assets/js/forms.js
+++ src/assets/js/forms.js
@@ -566,7 +566,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     try {
 
-      const res = await fetch(form.action, {
+      const res = await fetch(`https://kewan.dev/${form.action}`, {
         method: "POST",
         body: new FormData(form),
         headers: { Accept: "application/json" },
